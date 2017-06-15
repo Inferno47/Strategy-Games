94d6f6a756ab945f42014468e7b4bd752185c4cd
Inferno <aymeric.gand@gmail.com>
2017-06-15 13:19:26 +0200

    Add Menu
    Ajout des Menu Pricipale, Multi, Option
    
    Menu Option non fini
    
    Button Quiter fonctionelle

---
+++ Strategie_deux/Assets/Menu/MainMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Solo () {
+
+    }
+
+    public void MultiJoueur ()
+    {
+
+    }
+
+    public void Option ()
+    {
+
+    }
+
+    public void Quiter()
+    {
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
