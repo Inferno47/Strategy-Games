12d2d085008a64afa5b7c1bf6b20a24730cf9b3c
Inferno <aymeric.gand@gmail.com>
2017-06-26 23:35:38 +0200

    Menu Option
    - Recupere les valeurs dans le script c#: 12/12
    
    - Save les valeurs dans un XML: 12/12
    
    - Charger les valeurs depuis un XML: 12/12
    
    - Charger les valeurs dans unity: 12/12
    
    - Appliquer les valeurs dans unity: 11/12
    
    - Mettre les script du menu option au propre: En Cours

---
+++ Strategie_deux/Assets/Game/GameSettings.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+[XmlRoot("GameSettings")]
+public class GameSettings {
+
+	public bool vSync;
+	public bool fullScreen;
+	public bool HDR;
+
+	public int graphicQuality;
+	public int texturesQuality;
+	public int skyQuality;
+	public int shadowQuality;
+	public int antiAliasing;
+	public int resolutionIndex;
+	public int anisotropic;
+	public int methodAntiAliasing;
+	public int postProcessing;
+	public int buffer;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	public void DebugLog()
+	{
+		Debug.Log("GameSettings");
+		Debug.Log("Sync = " + vSync);
+		Debug.Log("HDR = " + HDR);
+		Debug.Log("FullScreen = " + fullScreen);
+		Debug.Log("Graphic = " + graphicQuality);
+		Debug.Log("Shadow = " + shadowQuality);
+		Debug.Log("Textures = " + texturesQuality);
+		Debug.Log("Sky = " + shadowQuality);
+		Debug.Log("AntiAliasing = " + (antiAliasing == 0 ? 0 : (int)Mathf.Pow(2, antiAliasing)));
+		Debug.Log("Resolution = " + resolutionIndex);
+		Debug.Log("Mode AntiAliasing = " + methodAntiAliasing);
+		Debug.Log("Anisotropic = " + (anisotropic == 0 ? 0 : (int)Mathf.Pow(2, anisotropic)));
+		Debug.Log("PostProcessing = " + postProcessing);
+		Debug.Log("Buffer = " + buffer);
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Strategie_deux/Assets/Menu/OptionMenuController.cs
+++ Strategie_deux/Assets/Menu/OptionMenuController.cs
@@ -1,325 +1,223 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
-
-struct Windows {
-	public int x;
-	public int y;
-}
-
-public enum SizeBuffer {
-	Simple,
-	Double,
-	Triple
-}
-
-public enum Aliasing {
-	None,
-	SSAA,
-	MSAA,
-	CSAA,
-	EQAA
-}
 
 public class OptionMenuController : MonoBehaviour {
 
-	private int graphic;
-	private int textures;
-	private int sky;
-	private int processing;
-	private int shadow;
-	private int antialiasing;
-	private int anisotropic;
-	private int windowMode;
-	private int indexResolution;
-	private bool syncrovertical;
-	private SizeBuffer buffer;
-	private Aliasing antiAliasing;
-	private List<Windows> resolution = null;
+	public Toggle fullScreenToggle;
+	public Toggle vSyncToggle;
+	public Toggle HDRToggle;
+	public Dropdown resolutionDropdown;
+	public Dropdown graphicDropdown;
+	public Dropdown textureDropdown;
+	public Dropdown skyDropdown;
+	public Dropdown shadowDropdown;
+	public Dropdown aaDropdown;
+	public Dropdown anisotropicDropdown;
 
-	public bool Syncrovertical
-	{
-		get
-		{
-			return syncrovertical;
-		}
+	public Dropdown postProcessingDropdown;
+	public Dropdown methodAntiAliasingDropdown;
+	public Dropdown bufferDropdown;
 
-		set
-		{
-			syncrovertical = value;
-		}
-	}
-
-	public int Graphic
-	{
-		get
-		{
-			return graphic;
-		}
-
-		set
-		{
-			graphic = value;
-		}
-	}
-
-	public int Textures
-	{
-		get
-		{
-			return textures;
-		}
-
-		set
-		{
-			textures = value;
-		}
-	}
-
-	public int Sky
-	{
-		get
-		{
-			return sky;
-		}
-
-		set
-		{
-			sky = value;
-		}
-	}
-
-	public int Processing
-	{
-		get
-		{
-			return processing;
-		}
-
-		set
-		{
-			processing = value;
-		}
-	}
-
-	public int Shadow
-	{
-		get
-		{
-			return shadow;
-		}
-
-		set
-		{
-			shadow = value;
-		}
-	}
-
-	public int Antialiasing
-	{
-		get
-		{
-			return antialiasing;
-		}
-
-		set
-		{
-			antialiasing = value;
-		}
-	}
-
-	public int WindowMode
-	{
-		get
-		{
-			return windowMode;
-		}
-
-		set
-		{
-			windowMode = value;
-		}
-	}
-
-	public int IndexResolution
-	{
-		get
-		{
-			return indexResolution;
-		}
-
-		set
-		{
-			indexResolution = value;
-		}
-	}
-
-	public int Anisotropic
-	{
-		get
-		{
-			return anisotropic;
-		}
-
-		set
-		{
-			anisotropic = value;
-		}
-	}
-
-	public int Buffer
-	{
-		get
-		{
-			return (int)buffer;
-		}
-
-		set
-		{
-			buffer = (SizeBuffer)value;
-		}
-	}
-
-	public int AntiAliasing
-	{
-		get
-		{
-			return (int)antiAliasing;
-		}
-
-		set
-		{
-			antiAliasing = (Aliasing)value;
-		}
-	}
+	public bool general;
+	private Resolution[] resolutions = null;
+	GameSettings Settings;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		CreateResolution();
+		if (File.Exists("Settings.xml"))
+			LoadSettings();
+		else
+			Settings = new GameSettings();
 	}
 
 	// Update is called once per frame
 	void Update() {
-	}
+	 }
 
 	private void CreateResolution()
 	{
-		resolution = new List<Windows>();
-		Windows size1 = new Windows();
-		size1.x = 640;
-		size1.y = 480;
-		resolution.Add(size1);
-		Windows size2 = new Windows();
-		size2.x = 720;
-		size2.y = 480;
-		resolution.Add(size2);
-		Windows size3 = new Windows();
-		size3.x = 720;
-		size3.y = 576;
-		resolution.Add(size3);
-		Windows size4 = new Windows();
-		size4.x = 800;
-		size4.y = 600;
-		resolution.Add(size4);
-		Windows size5 = new Windows();
-		size5.x = 1024;
-		size5.y = 768;
-		resolution.Add(size5);
-		Windows size6 = new Windows();
-		size6.x = 1280;
-		size6.y = 720;
-		resolution.Add(size6);
-		Windows size7 = new Windows();
-		size7.x = 1280;
-		size7.y = 768;
-		resolution.Add(size7);
-		Windows size8 = new Windows();
-		size8.x = 1280;
-		size8.y = 800;
-		resolution.Add(size8);
-		Windows size9 = new Windows();
-		size9.x = 1280;
-		size9.y = 960;
-		resolution.Add(size9);
-		Windows size10 = new Windows();
-		size10.x = 1280;
-		size10.y = 1024;
-		resolution.Add(size10);
-		Windows size11 = new Windows();
-		size11.x = 1360;
-		size11.y = 768;
-		resolution.Add(size11);
-		Windows size12 = new Windows();
-		size12.x = 1360;
-		size12.y = 1024;
-		resolution.Add(size12);
-		Windows size13 = new Windows();
-		size13.x = 1366;
-		size13.y = 768;
-		resolution.Add(size13);
-		Windows size14 = new Windows();
-		size14.x = 1440;
-		size14.y = 900;
-		resolution.Add(size14);
-		Windows size15 = new Windows();
-		size15.x = 1600;
-		size15.y = 900;
-		resolution.Add(size15);
-		Windows size16 = new Windows();
-		size16.x = 1680;
-		size16.y = 1050;
-		resolution.Add(size16);
-		Windows size17 = new Windows();
-		size17.x = 1920;
-		size17.y = 1080;
-		resolution.Add(size17);
-		Windows size18 = new Windows();
-		size18.x = 1920;
-		size18.y = 1200;
-		resolution.Add(size18);
+		Resolution Previous = new Resolution();
+		resolutions = Screen.resolutions;
+
+		foreach (Resolution resolution in resolutions)
+		{
+			if ((resolution.height != Previous.height) && (resolution.width != Previous.width))
+				resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+			Previous = resolution;
+		}
 	}
 
-	public void Load() {
-		//Load Configuration in XML
+	public void LoadSettings() {
+		XmlSerializer serializer = new XmlSerializer(typeof(GameSettings));
+		FileStream stream = new FileStream("Settings.xml", FileMode.Open);
+		Settings = serializer.Deserialize(stream) as GameSettings;
+		stream.Close();
+		
+		LoadUI();
+		
+		
 	}
 
-	public void Save () {
-		Debug.Log("Sync = " + syncrovertical);
-		Debug.Log("Graphic = " + graphic);
-		Debug.Log("Shadow = " + shadow);
-		Debug.Log("Textures = " + textures);
-		Debug.Log("Processing = " + processing);
-		Debug.Log("Buffer = " + buffer);
-		Debug.Log("Mode AntiAliasing = " + antiAliasing);
-		Debug.Log("Sky = " + sky);
-		Debug.Log("Antialisaing = " + x(antialiasing));
-		Debug.Log("Anisotropic = " + x(anisotropic));
-		Debug.Log("Resolution = " + resolution[indexResolution].x + "x" + resolution[indexResolution].y);
-		Debug.Log("Mode Windows = " + (windowMode == 0 ? true : false));
-		//Save Configuration in XML
+	public void LoadUI() {	
+		general = true;
+		fullScreenToggle.isOn = Settings.fullScreen;
+		vSyncToggle.isOn = Settings.vSync;
+		HDRToggle.isOn = Settings.HDR;
+		resolutionDropdown.value = Settings.resolutionIndex;
+		textureDropdown.value = Settings.texturesQuality;
+		skyDropdown.value = Settings.skyQuality;
+		shadowDropdown.value = Settings.shadowQuality;
+		postProcessingDropdown.value = Settings.postProcessing;
+		bufferDropdown.value = Settings.buffer;
+		methodAntiAliasingDropdown.value = Settings.methodAntiAliasing;
+		anisotropicDropdown.value = Settings.anisotropic;
+		aaDropdown.value = Settings.antiAliasing;
+		graphicDropdown.value = Settings.graphicQuality;
+		resolutionDropdown.RefreshShownValue();
+		
 	}
 
-	public int x (int nb) {
-		if (nb == 0)
-			return (0);
-		int result = 1;
-		for (int i = 0; i < nb; i++)
-			result *= 2;
-		return result;
-	}
 
+	public void SaveSettings () {
+		XmlSerializer serializer = new XmlSerializer(typeof(GameSettings));
+		FileStream stream = new FileStream("Settings.xml", FileMode.Create);
+		serializer.Serialize(stream, Settings);
+		stream.Close();
+	}
 
 	public void Retour () {
 		SceneManager.LoadScene("MainMenu");
 	}
 
 	public void Appliquer() {
-		QualitySettings.shadowResolution = (ShadowResolution)shadow;
-		QualitySettings.antiAliasing = x(antialiasing);
-		Screen.SetResolution(resolution[indexResolution].x, resolution[indexResolution].y, windowMode == 0 ? true : false);
-		//Application of options
-		Save();
+		Screen.SetResolution(resolutions[Settings.resolutionIndex].width, resolutions[Settings.resolutionIndex].height, Settings.fullScreen);
+		QualitySettings.shadowResolution = (ShadowResolution)Settings.shadowQuality;
+		QualitySettings.antiAliasing = (Settings.antiAliasing == 0 ? 0 : (int)Mathf.Pow(2, Settings.antiAliasing));
+		QualitySettings.masterTextureLimit = Settings.texturesQuality;
+		QualitySettings.vSyncCount = Settings.vSync == true ? 1 : 0;
+		QualitySettings.asyncUploadBufferSize = (Settings.buffer == 0 ? 0 : (int)Mathf.Pow(4, Settings.buffer));
+		Camera.current.allowHDR = Settings.HDR;
+		QualitySettings.anisotropicFiltering = AnisotropicFiltering.ForceEnable;
+		Texture.SetGlobalAnisotropicFilteringLimits(Settings.anisotropic , 9);
+		if (Settings.methodAntiAliasing == 1)
+			Camera.current.allowMSAA = true;
+		SaveSettings();
 		Retour();
+		//Application of options 11/12
+	}
+
+	public void UpdateAA(int postProcessing) {
+		if (postProcessing > 0 && methodAntiAliasingDropdown.options.Count == 2)
+		{
+			methodAntiAliasingDropdown.options.Add(new Dropdown.OptionData("TAA"));
+			methodAntiAliasingDropdown.options.Add(new Dropdown.OptionData("FXAA"));
+			methodAntiAliasingDropdown.options.Add(new Dropdown.OptionData("SSAA"));
+		}
+		else if (postProcessing == 0 && methodAntiAliasingDropdown.options.Count == 5)
+		{
+			methodAntiAliasingDropdown.options.RemoveRange(2, 3);
+			methodAntiAliasingDropdown.RefreshShownValue();
+		}
+	}
+
+	public void ModeCustom()
+	{
+		if (general == false)
+			graphicDropdown.value = 4;
+	}
+
+	public void SetFullScreen(bool enable)
+	{
+		Settings.fullScreen = enable;
+	}
+
+	public void SetVSync(bool enable)
+	{
+		Settings.vSync = enable;
+		ModeCustom();
+
+	}
+
+	public void SetHDR(bool enable)
+	{
+		Settings.HDR = enable;
+		ModeCustom();
+	}
+
+	public void SetResolution(int index) {
+		Settings.resolutionIndex = index;
+	}
+
+	public void SetGraphicQuality(int graphic) {
+		Settings.graphicQuality = graphic;
+		if (graphic < 4)
+		{
+			general = true;
+			textureDropdown.value = graphic;
+			skyDropdown.value = graphic;
+			shadowDropdown.value = graphic;
+			aaDropdown.value = graphic;
+			postProcessingDropdown.value = graphic;
+			anisotropicDropdown.value = graphic;
+			if (graphic > 1)
+				vSyncToggle.isOn = true;
+			else
+				vSyncToggle.isOn = false;
+			if (graphic > 2)
+				HDRToggle.isOn = true;
+			else
+				HDRToggle.isOn = false;
+			general = false;
+		}
+	}
+
+	public void SetTextureQuality(int texture) {
+		Settings.texturesQuality = texture;
+		ModeCustom();
+
+	}
+
+	public void SetSkyQuality(int sky) {
+		Settings.skyQuality = sky;
+		ModeCustom();
+	}
+
+	public void SetShadowQuality(int shadow) {
+		Settings.shadowQuality = shadow;
+		ModeCustom();
+	}
+
+	public void SetAA(int aa)
+	{
+		Settings.antiAliasing = aa;
+		ModeCustom();
+	}
+
+	public void SetPostProcessing(int postProcessing) {
+		Settings.postProcessing = postProcessing;
+		UpdateAA(postProcessing);
+		ModeCustom();
+	}
+
+	public void SetMethodAntiAliasing(int methodAntiAliasing)
+	{
+		Settings.methodAntiAliasing = methodAntiAliasing;
+		ModeCustom();
+	}
+	public void SetBuffer(int buffer)
+	{
+		Settings.buffer = buffer;
+		ModeCustom();
+	}
+
+	public void SetAnisotropic(int anisotropic) {
+		Settings.anisotropic = anisotropic;
+		ModeCustom();
 	}
 }
